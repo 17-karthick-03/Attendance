32ea743f17dcdc14b5e537fe07b6671f28bde1ce
KARTHICK S <143118223+17-karthick-03@users.noreply.github.com>
2024-07-06 19:28:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -167,7 +167,7 @@ document.addEventListener("DOMContentLoaded", function () {
         dob: "2005-03-17",
         name: "KARTHICK S",
         total_number: 10,
-        attend: 9,
+        attend: 5,
       },
       {
         regNo: "142222104074",
